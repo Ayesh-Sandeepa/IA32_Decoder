a5c176ad32034410b0cc8fe81b374b4d46690300
GayanJayakody <gayanjayakody13@gmail.com>
2021-10-19 01:22:29 +0530

    Create mov.h

---
+++ mov.h
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <bitset>
+#include <iostream>
+#include <string>
+#include <cmath>
+#include <sstream>
+#include <iomanip>
+#include <queue>
+#include <map>
+#include <list>
+
+#include "common.h"
+
+using namespace std;
+class Mov{
+    private:
+        bool opSize;
+        string regs_8[8];
+        string regs_16[8];
+        string regs_32[8];        
+    public:
+        Common common;
+        queue<short> &instruction;
+        map<string, int> &registers;
+        map<string, int> &memories32bit;
+        map<string, int16_t> &memories16bit;
+        map<string, int8_t> &memories8bit;
+        list<string> &memoryAccesses;
+
+        Mov(Common com, queue<short> &instruction, map<string, int> &registers, map<string, int> &memories32bit, map<string, int16_t> &memories16bit, map<string, int8_t> &memories8bit, list<string> &memoryAccesses);
+
+        string decode_mov(short prefixes[4]);
+        string decode_displacement_with_SIB(int w, int d, int mod, int reg, int index, int scale, int base);
+        string decode_displacement_without_SIB(int w, int d, int mod, int reg, int rm);
+        string decode_SIB(int w, int d, int mod, int reg);
+        string decode_mod_00(int w, int d, int reg, int rm);
+        string decode_mod_01(int w, int d, int reg, int rm);
+        string decode_mod_10(int w, int d, int reg, int rm);
+        string decode_mod_11(int w, int d, int reg, int rm);
+};

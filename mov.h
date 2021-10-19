52e7448bb8da4e79e06215f750a76645b987af3a
GayanJayakody <gayanjayakody13@gmail.com>
2021-10-19 23:39:00 +0530

    added immediate

--- mov.h
+++ mov.h
@@ -9,7 +9,7 @@
 #include <map>
 #include <list>
 
-#include "common.h"
+#include "mov_immediate.h"
 
 using namespace std;
 class Mov{
@@ -38,4 +38,35 @@ class Mov{
         string decode_mod_10(int w, int d, int reg, int rm);
         string decode_mod_11(int w, int d, int reg, int rm);
         string decode_imm_alt(int opCode, int w, int reg);
+};
+
+class Mov_addOverride
+{
+private:
+    bool opSize;
+    uint16_t *list1[2], *list2[2], *list3[4];
+    string regs_16bitmode[8];
+    string regs_8[8];
+    string regs_16[8];
+    string regs_32[8];
+public:
+    Common common;
+    queue<short> &instruction;
+    map<string, int> &registers;
+    map<string, int> &memories32bit;
+    map<string, int16_t> &memories16bit;
+    map<string, int8_t> &memories8bit;
+    list<string> &memoryAccesses;
+
+    Mov_addOverride(Common com, queue<short> &instruction, map<string, int> &registers, map<string, int> &memories32bit, map<string, int16_t> &memories16bit, map<string, int8_t> &memories8bit, list<string> &memoryAccesses);
+
+    string decode_add(short prefixes[4]);
+    string decode_displacement_with_SIB(int w, int d, int mod, int reg, int index, int scale, int base);
+    string decode_displacement_without_SIB(int w, int d, int mod, int reg, int rm);
+    string decode_SIB(int w, int d, int mod, int reg);
+    string decode_mod_00(int w, int d, int reg, int rm);
+    string decode_mod_01(int w, int d, int reg, int rm);
+    string decode_mod_10(int w, int d, int reg, int rm);
+    string decode_mod_11(int w, int d, int reg, int rm);
+    string decode_imm(int opCode, int w, int d);
 };

335a580d58cc41cbc1873b6d2a00443f9e47cc37
Ayesh Hallawaarachchi <42457319+Ayesh-Sandeepa@users.noreply.github.com>
2021-10-25 23:03:35 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -108,58 +108,52 @@ int main()
 
         nextOpcode = encoded_instructions.front();
 
-        // cout << "Next opcode : " << nextOpcode << "\n";
-
-        // common.listQueue(encoded_instructions);
-        // cout << "Next opcode:" << common.getHex(nextOpcode, 0, 0) << "\n";
         if (nextOpcode == 0xf0 or nextOpcode == 0xf2 or nextOpcode == 0xf3)
         {
-            // printf("Prefix 1 set\n");
+            
             prefixes[0] = nextOpcode;
             encoded_instructions.pop();
         }
         else if (nextOpcode == 0x2e or nextOpcode == 0x36 or nextOpcode == 0x3e or nextOpcode == 0x26 or nextOpcode == 0x64 or nextOpcode == 0x65)
         {
-            // printf("Prefix 2 set\n");
+            
             prefixes[1] = nextOpcode;
             encoded_instructions.pop();
         }
         else if (nextOpcode == 0x66)
         {
-            // printf("Prefix 3 set\n");
+            
             prefixes[2] = nextOpcode;
             encoded_instructions.pop();
         }
         else if (nextOpcode == 0x67)
         {
-            // printf("Prefix 4 set\n");
+            
             prefixes[3] = nextOpcode;
             encoded_instructions.pop();
         }
         else if (nextOpcode == 0x0f)
         {
             twoBytesOpcode = true;
-            // printf("Two byte opcode enables\n");
             encoded_instructions.pop();
         }
         else if (twoBytesOpcode == true and nextOpcode == 0x38)
         {
             threeBytesOpcode = true;
-            // printf("Three byte opcode enables\n");
             encoded_instructions.pop();
         }
         else
         {
             if (threeBytesOpcode)
             {
-                // printf("Three bytes opcode \n");
+                
             }
             else if (twoBytesOpcode)
             {
-                // printf("Two bytes opcode \n");
+                
                 if (nextOpcode == 0xa3 or nextOpcode == 0xba)
                 {
-                    // printf("Opcode a3 or ba \n");
+                    
                     bitset.decode_bt(prefixes);
                 }
             }
@@ -170,7 +164,7 @@ int main()
                 short modrm = encoded_instructions_temp.front();
                 int reg = common.get_bits(4, 3, modrm);
 
-                // printf("One byte opcode \n");
+                
                 if (nextOpcode == 0x0 or nextOpcode == 0x1 or nextOpcode == 0x2 or nextOpcode == 0x3 or nextOpcode == 0x4 or nextOpcode == 0x5 or ((nextOpcode == 0x80 or nextOpcode == 0x81 or nextOpcode == 0x83) and reg==0))
                 {
                     adder.decode_add(prefixes);
@@ -194,17 +188,14 @@ int main()
 
                 else if (nextOpcode == 0xfe or nextOpcode == 0xff or nextOpcode == 0x40 or nextOpcode == 0x48)
                 {
-                    // for both Increment and Decrement
                     incDec.decode_inc(prefixes);
                 }
                 else if (nextOpcode == 0xc0 or nextOpcode == 0xc1 or nextOpcode == 0xd0 or nextOpcode == 0xd1 or nextOpcode == 0xd2 or nextOpcode == 0xd3)
                 {
-                    //for both Left shift and Right shift
                     LeftRightShift.decode_shl(prefixes);
                 }
                 else if (nextOpcode == 0x20 or nextOpcode == 0x21 or nextOpcode == 0x22 or nextOpcode == 0x23 or nextOpcode == 0x24 or nextOpcode == 0x25 or ((nextOpcode == 0x80 or nextOpcode == 0x81 or nextOpcode == 0x83) and (reg == 4)))
                 {
-                   
                     and_.decode_and(prefixes);
                 }
                 else if (nextOpcode == 0x3c or nextOpcode == 0x3d or nextOpcode == 0x38 or nextOpcode == 0x39 or nextOpcode == 0x3a or nextOpcode == 0x3b or ((nextOpcode == 0x80 or nextOpcode == 0x81 or nextOpcode == 0x83) and (reg == 7))) {
